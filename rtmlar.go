f8a494af730db8884049dcc40ca674724e747e81
gilarwahibul <gilarwahibul@gmail.com>
2023-07-20 09:32:04 +0700

    p

--- rtmlar.go
+++ rtmlar.go
@@ -17,24 +17,24 @@ func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (inser
 }
 
 func InsertDataCS(db *mongo.Database, nama, email, nohp string, negara string, desc string) (InsertedID interface{}) {
-	var DataCS DataCS
-	DataCS.Nama = nama
-	DataCS.Email = email
-	DataCS.Nohp = nohp
-	DataCS.Negara = negara
-	DataCS.Desc = desc
-	return InsertOneDoc(db, "data_DataCS", DataCS)
+	var dataCS DataCS
+	dataCS.Nama = nama
+	dataCS.Email = email
+	dataCS.Nohp = nohp
+	dataCS.Negara = negara
+	dataCS.Desc = desc
+	return InsertOneDoc(db, "data_DataCS", dataCS)
 }
 
-func GetDataNegara(negara string, db *mongo.Database, col string) (data DataCS) {
-	act := db.Collection(col)
-	filter := bson.M{"negara": negara}
-	err := act.FindOne(context.TODO(), filter).Decode(&data)
-	if err != nil {
-		fmt.Printf("getdataanegara: %v\n", err)
-	}
-	return data
-}
+//	func GetDataNegara(negara string, db *mongo.Database, col string) (data DataCS) {
+//		act := db.Collection(col)
+//		filter := bson.M{"negara": negara}
+//		err := act.FindOne(context.TODO(), filter).Decode(&data)
+//		if err != nil {
+//			fmt.Printf("getdataanegara: %v\n", err)
+//		}
+//		return data
+//	}
 func GetDataNama(nama string, db *mongo.Database, col string) (data DataCS) {
 	accou := db.Collection(col)
 	filter := bson.M{"nama": nama}
@@ -44,16 +44,17 @@ func GetDataNama(nama string, db *mongo.Database, col string) (data DataCS) {
 	}
 	return data
 }
-func DeleteDataNegara(negara string, db *mongo.Database, col string) (data DataCS) {
-	dct := db.Collection(col)
-	filter := bson.M{"negara": negara}
-	err, _ := dct.DeleteOne(context.TODO(), filter)
-	if err != nil {
-		fmt.Printf("DeleteDataCS : %v\n", err)
-	}
-	fmt.Println("Succes Delete data")
-	return data
-}
+
+// func DeleteDataNegara(negara string, db *mongo.Database, col string) (data DataCS) {
+// 	dct := db.Collection(col)
+// 	filter := bson.M{"negara": negara}
+// 	err, _ := dct.DeleteOne(context.TODO(), filter)
+// 	if err != nil {
+// 		fmt.Printf("DeleteDataCS : %v\n", err)
+// 	}
+// 	fmt.Println("Succes Delete data")
+// 	return data
+// }
 
 func DeleteDataNama(nama string, db *mongo.Database, col string) (data DataCS) {
 	dena := db.Collection(col)
